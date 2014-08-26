westtoer/afwezig.westtoer.be

commit 892a6db7315977b3af57f7c151f3684251f358e5

Implemented CRUD feature on Calendar Days as requested - Issue #26

app/Controller/AdminController.php
@@ -741,17 +741,41 @@ class AdminController extends AppController {
     public function editCalendarDays(){
         $this->set('crud', false);
         $this->set('employees', $this->Employee->find('all', array('conditions' => array('Employee.internal_id <>' => '-1' ))));
+        $this->set('cit', $this->CalendarItemType->find('all'));
         if($this->request->is('post')){
-            $incomingCalendarDays = $this->request->data;
-            if($this->CalendarDay->saveMany($incomingCalendarDays)){
-                $this->Sesion->setFlash('Kalenderdagen opgeslagen.');
-            } else {
-                $this->Session->setFlash('Kalenderdagen konden niet worden opgeslagen.');
+            $icd = $this->request->data["items"]; // Incoming Calendar Days
+            $employee = $this->Employee->findById($this->request->data["Crud"]["employee_id"]);
+
+            foreach($icd as $date => $cd){
+                foreach($cd as $hour => $type){
+                    if(is_array($type)){
+                        foreach($type as $id => $object){
+                            if($id != 0){
+                                $vcd[] = array('CalendarDay' => array('id' => $id, 'employee_id' => $employee["Employee"]["id"],'day_date' => $date, 'day_time' => $hour, 'calendar_item_type_id' => $object["type"])); //Verified Calendar Days
+                            } else {
+                                if($object['type'] != 0){
+                                    $ncd[] = array('CalendarDay' => array('employee_id' => $employee["Employee"]["id"],'day_date' => $date, 'day_time' => $hour, 'calendar_item_type_id' => $object["type"])); //New Calendar Days
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            $this->redirect('/Admin');
+
+            if($this->CalendarDay->saveMany($vcd)){
+                if($this->CalendarDay->saveMany($ncd)){
+                    $this->Session->setFlash('Het opslaan van de kalender is geslaagd.');
+                } else {
+                    $this->Session->setFlash('Het opslaan van de nieuwe kalenderdagen is mislukt.');
+                }
+            } else {
+                $this->Session->setFlash('Het opslaan van de kalender is mislukt.');
+            }
+            $this->redirect($this->here);
         } else {
             if(isset($this->request->query['month'])){
                 $month = $this->request->query['month'];
+                if($month < 10){$niceMonth = '0' . $month;} else {$niceMonth = $month;}
                 $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, date('Y'));
                 if(isset($this->request->query['employee'])){
                     if(isset($this->request->query['year'])){
@@ -762,11 +786,31 @@ class AdminController extends AppController {
                     $employee = $this->request->query['employee'];
                     $this->set('crud', true);
 
-                    $this->set('calendarDays', $this->CalendarDay->find('all', array('conditions' => array(
-                        'day_date >=' => date('Y-m-d', strtotime($year . '-' . $month .'-01')),
-                        'day_date <=' => date('Y-m-d', strtotime($year . '-' . $month  . '-' . $daysInMonth)),
-                        'employee_id' => $employee
-                    ))));
+                    $date = array('start' => $year . '-' . $niceMonth .'-01', 'end' => $year . '-' . $niceMonth  . '-' . $daysInMonth);
+
+                    $ucd = $this->CalendarDay->find('all', array('conditions' => array(
+                        'day_date >=' => date('Y-m-d', strtotime($date["start"])),
+                        'day_date <=' => date('Y-m-d', strtotime($date["end"])),
+                        'employee_id' => $employee),
+                        'order' => 'day_date ASC'
+                    ));
+
+                    $template = $this->dateRange($date["start"], $date["end"]);
+
+                    foreach($template as $cd){
+                        $ocd[explode('/',$cd)[0]][explode('/',$cd)[1]] = array('id' => 0, 'name' => '', 'type_id' => 0);
+                    }
+
+                    foreach($ucd as $calendarDay){
+                        $ocd[$calendarDay["CalendarDay"]["day_date"]][$calendarDay["CalendarDay"]["day_time"]] = array('id' => $calendarDay["CalendarDay"]["id"], 'type_id' => $calendarDay["CalendarDay"]["calendar_item_type_id"], 'name' => $calendarDay["CalendarItemType"]["name"]);
+                    }
+
+                    if(isset($ocd)){
+                        $this->set('calendarDays', $ocd);
+                    } else {
+                        $this->set('calendarDays', 'Er is geen resultaat gevonden');
+                    }
+
                 }
             }
         }

app/View/Admin/edit_calendar_days.ctp
@@ -5,7 +5,8 @@
     <div class="col-md-9">
         <?php if($crud == true){?>
             <?php echo $this->Form->create('Crud', array('url' => $this->here));?>
-            <?php echo $this->Admin->crudCalendarDays($calendarDays);?>
+            <?php echo $this->Form->hidden('employee_id', array('value' => $this->request->query['employee']))?>
+            <?php echo $this->Admin->crudCalendarDays($calendarDays, $cit);?>
             <?php echo $this->Form->submit('Opslaan', array('class' => 'btn btn-primary fullwidth'));?>
             <?php echo $this->Form->end();?>
         <?php } else {?>

app/View/Helper/AdminHelper.php
@@ -1,7 +1,7 @@
 <?php
 class AdminHelper extends AppHelper {
 
-    public $helpers = array('Form');
+    public $helpers = array('Form', 'CalendarItemType');
 
     public function tableUsers($users, $type){
         $html = '<table class="table">';
@@ -189,26 +189,30 @@ class AdminHelper extends AppHelper {
         return $html;
     }
 
-    public function crudCalendarDays($calendarDays){
+    public function crudCalendarDays($calendarDays, $cit){
+        $types = $this->CalendarItemType->selectorAllCalendarItemTypes($cit, 'mixed');
         $html = '<table class="table table-bordered">';
-        $html .= '<tr><th>Dag</th><th>Uur</th><th>Type</th>';
-        foreach($calendarDays as $calendarDay){
+        $html .= '<tr><th width="200px">Dag</th><th>AM</th><th>PM</th>';
+
+        foreach($calendarDays as $date =>$cd){
             $html .= '<tr>';
-            $html .= '<td>';
-            $html .= '<input type="hidden" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][id]" value="'. $calendarDay["CalendarDay"]["id"] .'">';
-            $html .= '<input type="hidden" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][employee_id]" value="'. $calendarDay["CalendarDay"]["employee_id"] .'">';
-            $html .= '<input type="hidden" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][replacement_id]" value="'. $calendarDay["CalendarDay"]["replacement_id"] .'">';
-            $html .= '<input type="text" class="form-control" value="' . $calendarDay["CalendarDay"]["day_date"] . '" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][day_date]">';
-            $html .= '</td>';
-            $html .= '<td>';
-            $html .= '<input type="text" class="form-control" value="' . $calendarDay["CalendarDay"]["day_time"] . '" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][day_time]">';
-            $html .= '</td>';
-            $html .= '<td>';
-            $html .= '<input type="text" class="form-control" value="' . $calendarDay["CalendarDay"]["calendar_item_type_id"] . '" name="data[' . $calendarDay["CalendarDay"]["id"] .'][CalendarDay][calendar_item_type_id]">';
-            $html .= '</td>';
+            $html .= '<td>' . $date .'</td>';
+            foreach($cd as $hour => $desc){
+                $html .= '<td>';
+                $html .= '<select class="form-control" name="data[items][' . $date . '][' . $hour . '][' . $cd[$hour]["id"] . '][type]">';
+                $html .= '<option value="' . $cd[$hour]["type_id"] . '">' .$cd[$hour]["name"]  .'</option>';
+                foreach($types as $type){
+                    if($type["id"] !== $cd[$hour]["type_id"]){
+                        $html .= $type["html"];
+                    }
+                }
+                $html .= '</select>';
+                $html .= '</td>';
+            }
             $html .= '</tr>';
         }
         $html .= '</table>';
+
 
         return $html;
     }

commit 844c26272f70020405bed0b481a3c3eaad43bb8d

Fixed date limit in weekends caused by commit 892a6db - Issue #26

app/Controller/AdminController.php
@@ -797,6 +797,16 @@ class AdminController extends AppController {
 
                     $date = array('start' => $year . '-' . $niceMonth .'-01', 'end' => $year . '-' . $niceMonth  . '-' . $daysInMonth);
 
+                    if(date('D', strtotime($date["start"])) == "Sat" or date('D', strtotime($date["start"])) == "Sun"){
+                        $months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+                        $date["start"] = date('Y-m-d', strtotime('first monday of ' . $months[$month] . ' ' . date('Y')));
+                    }
+
+                    if(date('D', strtotime($date["end"])) == "Sat" or date('D', strtotime($date["end"])) == "Sun"){
+                        $months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+                        $date["end"] = date('Y-m-d', strtotime('last Friday of ' . $months[$month] . ' ' . date('Y')));
+                    }
+
                     $ucd = $this->CalendarDay->find('all', array('conditions' => array(
                         'day_date >=' => date('Y-m-d', strtotime($date["start"])),
                         'day_date <=' => date('Y-m-d', strtotime($date["end"])),