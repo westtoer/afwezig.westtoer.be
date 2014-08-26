c09f9a6b990418c627670742377c88695217f259
Niels Vermaut <nielsvermaut@gmail.com>
2014-08-26 11:23:51 +0200

    Added mixed type, echoing id and html now

--- app/View/Helper/CalendarItemTypeHelper.php
+++ app/View/Helper/CalendarItemTypeHelper.php
@@ -4,16 +4,23 @@ class CalendarItemTypeHelper extends AppHelper{
     public function selectorAllCalendarItemTypes($calendaritemtypes, $datatype = 'html'){
         //Give a standard null option
         if($datatype == 'html'){
-
             $CITOptions[] = '<option value="9">Gewerkt</option>';
-
-            //Fill with all CalendarItemTypes
             foreach($calendaritemtypes as $calendaritemtype){
                 if($calendaritemtype["CalendarItemType"]["id"] != 9){
                     $CITOptions[] = '<option value="' .$calendaritemtype["CalendarItemType"]["id"] . '">' . $calendaritemtype["CalendarItemType"]["name"] . '</option>';
                 }
             }
         }
+
+        //Fill with all CalendarItemTypes
+        if($datatype == 'mixed'){
+            foreach($calendaritemtypes as $calendaritemtype){
+                if($calendaritemtype["CalendarItemType"]["id"] != 9){
+                    $CITOptions[] = array('id' => $calendaritemtype["CalendarItemType"]["id"], 'html' =>'<option value="' .$calendaritemtype["CalendarItemType"]["id"] . '">' . $calendaritemtype["CalendarItemType"]["name"] . '</option>');
+                }
+            }
+        }
+
 
         return $CITOptions;
     }
