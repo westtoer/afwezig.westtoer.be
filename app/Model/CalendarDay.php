7cdc52ca7540a5346705d300e54e034258ae5d7f
Niels Vermaut <nielsvermaut@gmail.com>
2014-08-26 10:13:42 +0200

    Changed misconfigured JOIN causing no records to be found when no replacement was set - Fixed issue #47

--- app/Model/CalendarDay.php
+++ app/Model/CalendarDay.php
@@ -3,7 +3,7 @@ class CalendarDay extends AppModel{
     public $belongsTo = array(
         'CalendarItemType',
         'Employee',
-        'Replacement' => array('className' => 'Employee', 'type' => 'INNER', 'foreignKey' => 'replacement_id'),
+        'Replacement' => array('className' => 'Employee', 'foreignKey' => 'replacement_id'),
 
     );
     public $hasMany = array(
