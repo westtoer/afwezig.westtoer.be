6075940f4c3d2982830bed57203a5816e8323d35
Niels Vermaut <nielsvermaut@gmail.com>
2014-08-27 11:05:17 +0200

    Added global var export dir

--- app/Config/Administrator.php
+++ app/Config/Administrator.php
@@ -1,3 +1,5 @@
 <?php
 $config["Administrator"]["email"] = 'niels.vermaut@westtoer.be';
 $config["Administrator"]["base_fallback_url"] = 'http://afwezig.westtoer.be';
+//No trailing slash
+$config["Administrator"]["export_dir"] = '';
