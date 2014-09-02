8734a78f563897b0c9f906d87cad6985822271bb
Niels Vermaut <nielsvermaut@gmail.com>
2014-09-02 16:14:22 +0200

    Added stub of administrator.php

--- app/Config/Administrator.php
+++ app/Config/Administrator.php
@@ -1,5 +1,5 @@
 <?php
 $config["Administrator"]["email"] = 'niels.vermaut@westtoer.be';
-$config["Administrator"]["base_fallback_url"] = 'http://afwezig.westtoer.be';
+$config["Administrator"]["base_fallback_url"] = 'http://localhost/';
 //No trailing slash
 $config["Administrator"]["export_dir"] = '';
